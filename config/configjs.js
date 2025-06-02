1fd58870617e4d3d2a06ad1b770ab8353b11b51f
MbolaOdon <mbolaodon15@gmail.com>
2025-06-02 11:50:16 +0300

    cc

--- config/configjs.js
+++ config/configjs.js
@@ -13,7 +13,7 @@ module.exports = {
     }
   },
   test: {
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: "DATABASE_URL",
     password:"npg_aYe14qVOoQs",
     dialect: 'postgres',
     dialectOptions: {
@@ -24,7 +24,7 @@ module.exports = {
     }
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: "DATABASE_URL",
     password:"npg_aYe14qVOoQs",
     dialect: 'postgres',
     dialectOptions: {
